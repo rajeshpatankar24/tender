2b2d5b1ca9d6500722adda633652b6791e771621
Rajesh Patankar <rajeshpatankar24@gmail.com>
2025-10-11 18:21:34 +0530

    Redeploy without changes

--- tender-new/src/API_URL.js
+++ tender-new/src/API_URL.js
@@ -1,5 +1,11 @@
-export const __urlapi = "http://localhost:3001/user/";
-export const __categoryapiurl = "http://localhost:3001/category/";
-export const __subcategoryapiurl = "http://localhost:3001/subcategory/";
-export const __productapiurl ="http://localhost:3001/product/";
-export const __bidproductapiurl = "http://localhost:3001/bid/";
+// export const __urlapi = "http://localhost:3001/user/";
+// export const __categoryapiurl = "http://localhost:3001/category/";
+// export const __subcategoryapiurl = "http://localhost:3001/subcategory/";
+// export const __productapiurl ="http://localhost:3001/product/";
+// export const __bidproductapiurl = "http://localhost:3001/bid/";
+
+export const __urlapi = "https://tender-3-m1ej.onrender.com/user/";
+export const __categoryapiurl = "https://tender-3-m1ej.onrender.com/category/";
+export const __subcategoryapiurl = "https://tender-3-m1ej.onrender.com/subcategory/";
+export const __productapiurl = "https://tender-3-m1ej.onrender.com/product/";
+export const __bidproductapiurl = "https://tender-3-m1ej.onrender.com/bid/";
