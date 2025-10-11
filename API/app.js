8c03094fba19c2e2ecdc82644642c558a28dd56a
Rajesh Patankar <rajeshpatankar24@gmail.com>
2025-10-12 01:52:50 +0530

    updates files

--- API/app.js
+++ API/app.js
@@ -1,8 +1,10 @@
 import cors from 'cors';
 import express from 'express';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv'
 import expressFileupload from 'express-fileupload';
-import { configDotenv } from 'dotenv';
+// import { configDotenv } from 'dotenv';
+dotenv.config()
 const app = express();
 // to link route
 import userRouter from './router/user.router.js';
@@ -10,6 +12,9 @@ import categoryRouter from './router/category.router.js';
 import subcategoryRouter from './router/subCategory.router.js'
 import productRouter from './router/product.router.js';
 import bidRouter from './router/bid.router.js'
+import newsletterModel from './model/newsletter.model.js';
+import contactRouter from './model/contact.model.js'
+
 // for crose origin ( different port handle)
 app.use(cors());
 // route level middleware 
@@ -22,6 +27,8 @@ app.use('/category',categoryRouter);
 app.use("/subcategory",subcategoryRouter);
 app.use("/product",productRouter);
 app.use("/bid",bidRouter);
+app.use('/newsletter', newsletterModel);
+app.use('/contact', contactRouter);
 
 const PORT= process.env.PORT
 app.listen(PORT);
