b5e5e0751a8328f83268f7bfea3ce0c2c41e874b
Rajesh <rajeshpatankar24@gmail.com>
2025-10-11 13:29:04 +0530

    Delete src directory

--- src/API_URL.js
+++
@@ -1,5 +0,0 @@
-export const __urlapi = "http://localhost:3001/user/";
-export const __categoryapiurl = "http://localhost:3001/category/";
-export const __subcategoryapiurl = "http://localhost:3001/subcategory/";
-export const __productapiurl ="http://localhost:3001/product/";
-export const __bidproductapiurl = "http://localhost:3001/bid/";
