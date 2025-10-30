de4c710a942f9de40fd5d480acd36e28b9b1320a
Rajesh Patankar <rajeshpatankar24@gmail.com>
2025-10-30 22:05:19 +0530

    update URL file

---
+++ src/API_URL.js
@@ -0,0 +1,8 @@
+
+export const __urlapi = "https://tender-1-j2d4.onrender.com/user/";
+export const __categoryapiurl = "https://tender-1-j2d4.onrender.com/category/";
+export const __subcategoryapiurl = "https://tender-1-j2d4.onrender.com/subcategory/";
+export const __productapiurl = "https://tender-1-j2d4.onrender.com/product/";
+export const __bidproductapiurl = "https://tender-1-j2d4.onrender.com/bid/";
+export const __contactapiurl = "https://tender-1-j2d4.onrender.com/contact/"
+export const __newsletterapiurl = "https://tender-1-j2d4.onrender.com/newsletter/"
