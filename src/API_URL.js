e501604b59cf4ffb8e38508200ebabe52a0e1f71
Rajesh Patankar <rajeshpatankar24@gmail.com>
2025-11-10 22:17:52 +0530

    updated

--- src/API_URL.js
+++ src/API_URL.js
@@ -1,4 +1,4 @@
-
+// this is endpoints
 export const __urlapi = "https://tender-1-j2d4.onrender.com/user/";
 export const __categoryapiurl = "https://tender-1-j2d4.onrender.com/category/";
 export const __subcategoryapiurl = "https://tender-1-j2d4.onrender.com/subcategory/";
